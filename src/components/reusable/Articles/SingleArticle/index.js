e5abee8ec6463d70133cedc0b8cdbb44a65b173e
Martin <m.h.nazaryan@gmail.com>
2021-06-11 17:01:52 +0400

    Added moment

--- src/components/reusable/Articles/SingleArticle/index.js
+++ src/components/reusable/Articles/SingleArticle/index.js
@@ -2,13 +2,14 @@ import { Card } from 'antd';
 import { Image } from 'antd';
 
 import './styles.scss';
+import moment from 'moment';
 
 const SingleArticle = ({ article }) => (
   <Card>
     <Card.Meta
       avatar={<Image width={200} src={article.urlToImage} />}
       title={article.author}
-      description={article.publishedAt}
+      description={moment(article.publishedAt).format('MMM d Y')}
     />
   </Card>
 );
